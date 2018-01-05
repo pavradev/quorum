cfb43410fff6443da59e18589f0816486187e080
pavel-r <rabeckijps@gmail.com>
2018-01-06 00:57:52 +0100

    Use EIP signature for Metamask

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -40,9 +40,9 @@ type sigCache struct {
 
 // MakeSigner returns a Signer based on the given chain config and block number.
 func MakeSigner(config *params.ChainConfig, blockNumber *big.Int) Signer {
-	if config.IsQuorum {
-		return HomesteadSigner{}
-	}
+	//if config.IsQuorum {
+	//	return HomesteadSigner{}
+	//}
 	var signer Signer
 	switch {
 	case config.IsEIP155(blockNumber):
